25b5986881be8259b7c40a9a1ad0574e9ab6a31f
lijinglin <nextuser@163.com>
2025-10-26 14:20:09 +0800

    clap 3.0 kvpair parse

--- ch04/http_pie/src/main.rs
+++ ch04/http_pie/src/main.rs
@@ -1,18 +1,24 @@
+use anyhow::{Result, anyhow};
 use clap::Parser;
+use reqwest::Url;
+
 #[derive(Parser, Debug)]
 struct Get {
-    #[arg(value_parser = parse_url)]
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
 }
+
+// get 子命令
 
 #[derive(Parser, Debug)]
 struct Post {
     // HTTP请求的URL（例如：https://example.com）
-    #[arg(value_parser = parse_url)]
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
     /// HTTP请求的body参数，格式为key=value（可多个）
     /// 例如：name=Alice age=30
-    body: Vec<String>,
+    #[clap(parse(try_from_str = parse_kv_pair))]
+    body: Vec<KvPair>,
 }
 
 #[derive(Parser, Debug)]
@@ -22,17 +28,54 @@ enum HttpCommand {
 }
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[clap(author, version, about, long_about = None)]
 struct Opts {
-    #[command(subcommand)]
+    #[clap(subcommand)]
     http_cmd: HttpCommand,
 }
-use anyhow::Result;
-use reqwest::Url;
+
 //use std::error::Error;
 fn parse_url(s: &str) -> Result<String> {
     let _url: Url = Url::parse(s)?;
     Ok(s.into())
+}
+#[derive(Debug, PartialEq)]
+struct KvPair {
+    key: String,
+    value: String,
+}
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    s.parse()
+    // let mut split = s.split("=");
+
+    // let key = split
+    //     .next()
+    //     .ok_or_else(|| anyhow!("invalid key-value pair: {}", s))?;
+    // let value = split
+    //     .next()
+    //     .ok_or_else(|| anyhow!("invalid key-value pair: {}", s))?;
+
+    // Ok(KvPair {
+    //     key: key.into(),
+    //     value: value.into(),
+    // })
+}
+
+use std::str::FromStr;
+
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split("=");
+
+        let err_fn = || anyhow!(format!("Failed to parse {}", s));
+        let key = split.next().ok_or_else(err_fn)?.to_string();
+        let value = split.next().ok_or_else(err_fn)?.to_owned();
+        Ok(Self {
+            key: key.into(),
+            value: value.into(),
+        })
+    }
 }
 
 #[test]
