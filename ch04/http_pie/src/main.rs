1290b7bacf029a09e6db55e0529f8785be65a840
lijinglin <nextuser@163.com>
2025-10-26 10:53:19 +0800

    add value_parser

--- ch04/http_pie/src/main.rs
+++ ch04/http_pie/src/main.rs
@@ -7,6 +7,7 @@ struct Get {
 #[derive(Parser, Debug)]
 struct Post {
     // HTTP请求的URL（例如：https://example.com）
+    #[arg(value_parser = parse_url)]
     url: String,
     /// HTTP请求的body参数，格式为key=value（可多个）
     /// 例如：name=Alice age=30
@@ -25,6 +26,22 @@ struct Opts {
     #[command(subcommand)]
     http_cmd: HttpCommand,
 }
+use anyhow::Result;
+use reqwest::Url;
+//use std::error::Error;
+fn parse_url(s: &str) -> Result<String> {
+    let _url: Url = Url::parse(s)?;
+    Ok(s.into())
+}
+
+#[test]
+fn test_parse_url() {
+    let mut url = parse_url("https://www.google.com");
+    println!("{:?}", url);
+
+    url = parse_url("https://abc.com");
+    println!("{:?}", url);
+}
 
 /**
  * usage
@@ -33,4 +50,13 @@ struct Opts {
 fn main() {
     let opts = Opts::parse();
     println!("parsed args : {:?}", &opts);
+
+    match opts.http_cmd {
+        HttpCommand::Get(Get { url }) => {
+            println!("http cmd get url: {}", url);
+        }
+        HttpCommand::Post(Post { url, body }) => {
+            println!("http cmd post url: {}, body: {:?}", url, body);
+        }
+    }
 }
